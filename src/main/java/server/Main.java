8610473bf4c2e9e16c096df182e833d7f0f7c0f8
Prakhar Saxena <2020csb1111@iitrpr.ac.in>
2023-04-20 01:52:02 +0530

    Prakhar working

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -2,6 +2,10 @@ package server;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+
+        System.out.println(System.getProperty("java.version"));
+        System.out.println();
     }
 }
