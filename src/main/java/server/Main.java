e27dac6b3a8ff9c17d58d725fbbf58c0648e4377
Jugal <jugalchap123@gmail.com>
2023-04-20 01:50:55 +0530

    Chal raha less gooo ....

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -2,8 +2,7 @@ package server;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello World");
+        System.out.println("Jugal Working");
         System.out.println(System.getProperty("java.version"));
     }
 }
