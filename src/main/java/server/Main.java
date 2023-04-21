7b4377f7e75ebad4fd09d1c4548a64c528130701
Sanyam <2020csb1122@iitrpr.ac.in>
2023-04-21 18:02:51 +0530

    Sanyam ka chal gaya

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -2,7 +2,7 @@ package server;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Jugal and Tanuj and Prakhar Working");
+        System.out.println("Jugal and Tanuj and Prakhar and Sanyam Working");
         System.out.println(System.getProperty("java.version"));
         System.out.println("T-Dawg");
     }
